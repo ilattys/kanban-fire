5fdfadadc91830b6c847a577e2df9da118aa43af
lattiia <india.lattimore@dillards.com>
2023-04-14 11:22:49 -0500

    initial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA4RkmBATm-IejPcJWSCtpA_m0nbtIc1-Y",
+    authDomain: "kanbanfire-1d3eb.firebaseapp.com",
+    projectId: "kanbanfire-1d3eb",
+    storageBucket: "kanbanfire-1d3eb.appspot.com",
+    messagingSenderId: "837361083213",
+    appId: "1:837361083213:web:145aad11dd9ac1a74e4d04",
+    measurementId: "G-97DYRK7CCJ"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA4RkmBATm-IejPcJWSCtpA_m0nbtIc1-Y",
+    authDomain: "kanbanfire-1d3eb.firebaseapp.com",
+    projectId: "kanbanfire-1d3eb",
+    storageBucket: "kanbanfire-1d3eb.appspot.com",
+    messagingSenderId: "837361083213",
+    appId: "1:837361083213:web:145aad11dd9ac1a74e4d04",
+    measurementId: "G-97DYRK7CCJ"
+  }
+};
+
+
+
